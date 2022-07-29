91b02b1100472f593e332a8b505afa1592761c56
Amit Chavda <amitchavda9879@gmail.com>
2022-07-29 18:41:17 +0530

    Updated Card.js: Style changed

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
-
 export default function Card(props) {
+    let anchotStyle = { fontSize: "11px", color: "gray", marginLeft: "10px" }
+    let paraStyle = { height: "100%", fontSize: "12px", textAlign: "justify" }
 
-    console.log(props.item);
     return (
         <>
             <section className='FlexContainer'>
@@ -10,12 +10,12 @@ export default function Card(props) {
                 </div>
                 <div className="div2">
                     <span>{props.item.country}</span>
-                    <a style={{ fontSize: "11px", color: "gray", marginLeft: "10px" }} href="">Vew On Google Map</a>
+                    <a style={anchotStyle} href="">Vew On Google Map</a>
                     <h2>{props.item.city}</h2>
 
-                    <span style={{ fontSize: "14px" }}>{props.item.from} - {props.item.to}</span>
-                    <p style={{ height: "100%", fontSize: "12px", textAlign: "justify" }}>
-                    {props.item.desc}
+                    <span style={{ fontSize: "14px", fontWeight: "bold" }}>{props.item.from} - {props.item.to}</span>
+                    <p style={paraStyle}>
+                        {props.item.desc}
                     </p>
                 </div>
             </section>
